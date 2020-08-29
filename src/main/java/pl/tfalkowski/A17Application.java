9bec904c1e3e993edf9eb35ee31ca1fdee9cd8a4
Tomasz Falkowski <t.falkowski@hotmail.com>
2020-08-30 00:19:32 +0200

    Initial commit

---
+++ src/main/java/pl/tfalkowski/A17Application.java
@@ -0,0 +1,13 @@
+package pl.tfalkowski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A17Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/tfalkowski/A17ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tfalkowski;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
